638cc0ecc4cfc2fd6f46330c189135f8597b696b
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-22 15:42:36 +0700

    Finish interface segregation

--- src/com/phucdevs/solid/CubeSolid.java
+++ src/com/phucdevs/solid/CubeSolid.java
@@ -3,10 +3,15 @@ package com.phucdevs.solid;
 /**
  * @author Phuc.Le
  */
-public class CubeSolid implements Shape {
+public class CubeSolid implements Shape, ThreeDimensionalShape {
 
     @Override
     public double area() {
         return 100;
+    }
+
+    @Override
+    public double volume() {
+        return 0;
     }
 }
